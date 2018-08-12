f683355c4572c6be680d5607bda5c624e5a90a61
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-11 23:56:53 -0300

    Frontend -  Estrutura Inicial Build

---
+++ angular1/gulpTasks/deps.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+gulp.task('deps',['deps.js', 'deps.css','deps.assets'])
+
+gulp.task('deps.js', ()=>{
+
+})
+
+gulp.task('deps.css', ()=>{
+    
+})
+
+gulp.task('deps.assets', ()=>{
+    
+})
---
+++ angular1/gulpTasks/server.js
@@ -0,0 +1,15 @@
+const gulp = require ('gulp')
+//const watch = require('gulp-watch')
+//const webserver = require('gulp-webserver')
+  /*
+  Essa task vai ficar monitorando os arquivos no disco 
+  sempre que fizer alteração ( nos arquivos ) ela vai executar atualização
+  */
+
+gulp.task('watch', function() {
+
+})
+
+gulp.task('server', [watch], function () {
+
+})
