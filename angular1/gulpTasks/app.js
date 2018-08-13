1d253e31002460d9c82e279a027fba14d02c58c7
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-12 22:53:30 -0300

    Frontend - Build dos Arquivos do Projeto.

--- angular1/gulpTasks/app.js
+++ angular1/gulpTasks/app.js
@@ -1,18 +1,45 @@
-const gulp = require ('gulp')
-// uma task chamar outras tasks antes
-gulp.task('app', ['app.html', 'app.css', 'app.js','app.assets'])
-gulp.task('app.html', () => {
 
+// Carregar gulp e os plugins através da função `require` do nodejs
+const gulp = require ('gulp')
+
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
+
+// uma task chamar outras tasks antes
+//Nova tarefa através do ´gulp.task´ com o nome 'app', importante a sequence por isso utilizamos a sequence
+gulp.task('app', ['app.html', 'app.css', 'app.js','app.assets'])
+
+// Nova tarefa através do ´gulp.task´ com o nome 'app.html'
+// Renomear o arquivo que sera minificado e logo depois o minificamos com o `uglify`
+// `gulp.dest` para colocar os arquivos concatenados e minificados na pasta public
+gulp.task('app.html', () => {
+    return gulp.src('app/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(gulp.dest('public'))
 })
 
 gulp.task('app.css', () => {
-
+  return gulp.src('app/**/*.css')
+    .pipe(uglifycss({"uglyComments": true}))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('app.js', () => {
-
+  return  gulp.src('app/**/*.js')
+    .pipe(babel({presets: ['env']}))
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('public/assets/js'))
 })
 
 gulp.task('app.assets', () => {
-
+  return gulp.src('assets/**/*.*')
+    .pipe(gulp.dest('public/assets'))
 })
+
+
+
--- angular1/gulpTasks/deps.js
+++ angular1/gulpTasks/deps.js
@@ -1,13 +1,17 @@
+// Carregar gulp e os plugins através da função `require` do nodejs
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
 const uglifycss = require('gulp-uglifycss')
 const concat = require('gulp-concat')
-
-
+//Nova tarefa através do ´gulp.task´ com o nome 'deps', importante a sequence
 gulp.task('deps',['deps.js', 'deps.css','deps.assets'])
 
+// Nova tarefa através do ´gulp.task´ com o nome 'deps.js'
+// Carregar os arquivos que a gente quer rodar as tarefas com o `gulp.src`
+// logo depois usamos o `pipe` para rodar a tarefa uglify
+// `gulp.dest` para colocar os arquivos concatenados e minificados na pasta public/assets/js
 gulp.task('deps.js', function() {
-    gulp.src([
+    return gulp.src([
       'node_modules/angular/angular.min.js',
       'node_modules/angular-ui-router/release/angular-ui-router.min.js',
       'node_modules/angular-animate/angular-animate.min.js',
@@ -21,9 +25,12 @@ gulp.task('deps.js', function() {
     .pipe(concat('deps.min.js'))
     .pipe(gulp.dest('public/assets/js'))
   })
-  
+  // Nova tarefa através do ´gulp.task´ com o nome 'deps.css'
+  // Renomear o arquivo que sera minificado e logo depois o minificamos com o `uglifycss`
+  // Rodar uma tarefa para concatenação
+  // `gulp.dest` para colocar os arquivos concatenados e minificados na pasta public/assets/css
   gulp.task('deps.css', function() {
-    gulp.src([
+    return gulp.src([
       'node_modules/angular-toastr/dist/angular-toastr.min.css',
       'node_modules/font-awesome/css/font-awesome.min.css',
       'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
@@ -36,7 +43,7 @@ gulp.task('deps.js', function() {
   })
   
   gulp.task('deps.fonts', function() {
-    gulp.src([
+    return gulp.src([
       'node_modules/font-awesome/fonts/*.*',
       'node_modules/admin-lte/bootstrap/fonts/*.*'
     ])
