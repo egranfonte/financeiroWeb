52f0ca06b90441f5ab237b43c9beac2c074cd5f5
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-12 23:18:41 -0300

    Frontend - Configurando o Servidor com Reload Automático

--- angular1/gulpTasks/server.js
+++ angular1/gulpTasks/server.js
@@ -1,15 +1,26 @@
 const gulp = require ('gulp')
-//const watch = require('gulp-watch')
-//const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
   /*
   Essa task vai ficar monitorando os arquivos no disco 
   sempre que fizer alteração ( nos arquivos ) ela vai executar atualização
   */
-
+gulp.task('server', ['watch'], function () {
+  return gulp.src('public'.pipe(webserver({
+    livereload: true,
+    port: 3000,
+    open: true
+  })))
+})
 gulp.task('watch', function() {
+    watch('app/**/*.html', () => gul.start('app.html'))
+    watch('app/**/*.css', () => gulp.start('app.css'))
+    watch('app/**/*.js', () => gulp.start('app.js'))
+    watch('assets/**/*.*', () => gulp.start('app.assets'))
 
 })
 
-gulp.task('server', [watch], function () {
 
-})
+
+
+
