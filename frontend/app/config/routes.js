dba8ae857577065cac3df5ba49524704e51fce00
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-13 23:49:00 -0300

    Frontend - Configurando as Rotas com UI Router

---
+++ frontend/app/config/routes.js
@@ -0,0 +1,15 @@
+//configuração das minhas rotas
+angular.module('financeiroWeb').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('cicloPagamento', {
+            url: "/cicloPagamento",
+            templateUrl: "cicloPagamento/tabs.html"
+        })
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+])
