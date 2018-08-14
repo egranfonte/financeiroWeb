1b8db690448846c5f274f0eb3c3e173e8e8a9da3
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-14 00:39:02 -0300

    Frontend - Componente de Cabeçalho do Conteúdo

---
+++ frontend/app/common/components/contentHeader.js
@@ -0,0 +1,13 @@
+//meu componente contentHeader
+angular.module('financeiroWeb').component('contentHeader', {
+    bindings :{
+        name: '@',
+        small: '@',
+    },
+    template: `
+    <section class="content-header">
+        <h1> {{ $ctrl.name }} <small> {{ $ctrl.small }} </small></h1>
+    </section>
+    `
+})
+
