488e02b864e02436dbb8adfe1664a44953342682
Alfredo Egranfonte <egranfont@gmail.com>
2018-08-14 03:22:07 -0300

    Habilitando o CORS na Aplicação Backend

--- frontend/app/dashboard/dashboardController.js
+++ frontend/app/dashboard/dashboardController.js
@@ -10,10 +10,10 @@
         vm.getSumariza = function(){
             const url = 'http://localhost:3003/api/sumarizaPagamento'
             $http.get(url).then(function(response){
-                const {credit = 0, debt =0} = response.data      
-                vm.credit = credit
-                vm.debt = debt
-                vm.total = credit - debt
+                const data = response.data      
+                vm.credit = data.credit
+                vm.debt = data.debt
+                vm.total = data.credit - data.debt
             })
         }
         vm.getSumariza()
